16e57feef7a468f614f5102d766d1ae129ed48b1
gleb-hleb <ggleb9270@gmail.com>
2020-12-27 12:42:59 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './index.css';
+
+import App from './components/app';
+
+import store from './redux/store';
+
+// DEV ONLY!!!
+window.store = store;
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
